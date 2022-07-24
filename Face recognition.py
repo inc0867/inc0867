8bffec1e10f3e4e2e9815148ca92ddf272cfc7e0
inc0867 <109675242+inc0867@users.noreply.github.com>
2022-07-24 10:22:35 +0300

    Create  Face recognition.py

---
+++ Face recognition.py
@@ -0,0 +1,31 @@
+import face_recognition as inc
+import cv2 as incc
+
+
+#note : (pip install dlib , pip install face-recognition) #note2 if you can't download try these (pip install cmake , pip install wheel , pip install dlib , pip install face-recognition
+
+
+yol = "Your video .mp4"
+
+video = incc.VideoCapture(0)   #Web cam ---> 0 #video ----> yol
+color = (0,255,0) #rgb color "green"
+
+while True:
+    oo,kare = video.read()
+
+    lokasyon = inc.face_locations(kare)
+
+    for index,fl in enumerate(lokasyon):
+        topleftY,bottomRightX,bottomrightY,topleftX = fl 
+        pt1 = (topleftX,topleftY)
+        pt2 = (bottomRightX,bottomrightY)
+
+        incc.rectangle(kare,pt1,pt2,color)
+        incc.imshow("Tespit",kare)
+
+    a = incc.waitKey(1)
+
+    if a == 27:   #click esc for close
+        break
+video.release()
+incc.destroyAllWindows()
